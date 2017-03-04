d2dfc35512692b4b4e123e1d153cd96a020279e4
lisx@healthmall.cn <lisx@healthmall.cn>
2017-03-04 17:50:22 +0800

    'mui熟悉'

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/3/4.
+ */
+var indexObj = {
+    init:function(){
+        //传入toggle参数，用户无需关心当前是显示还是隐藏状态，mui会自动识别处理；
+        //mui('#sheet1').popover('toggle');
+    }
+}
+
+exports.indexObj = indexObj;
---
+++ static/plugins/WebViewJavascriptBridge.js
@@ -0,0 +1,25 @@
+/*1.这段代码是固定的，必须要放到js中*/
+function setupWebViewJavascriptBridge(callback) {
+    if (window.WebViewJavascriptBridge) {
+        return callback(WebViewJavascriptBridge);
+    }
+    if (window.WVJBCallbacks) {
+        return window.WVJBCallbacks.push(callback);
+    }
+    window.WVJBCallbacks = [callback];
+    var WVJBIframe = document.createElement('iframe');
+    WVJBIframe.style.display = 'none';
+    WVJBIframe.src = 'wvjbscheme://__BRIDGE_LOADED__';
+    document.documentElement.appendChild(WVJBIframe);
+
+    var ua = navigator.userAgent.toLowerCase();
+    if (/android/.test(ua)) {
+        document.addEventListener('WebViewJavascriptBridgeReady', function () {
+            callback(WebViewJavascriptBridge)
+        }, false);
+    }
+
+    setTimeout(function () {
+        document.documentElement.removeChild(WVJBIframe)
+    }, 0)
+}
